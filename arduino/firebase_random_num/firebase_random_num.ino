5b3349ee3af747954e4b6729c44912b2468aa086
Mark Deegan <1252943+markdeegan@users.noreply.github.com>
2025-12-03 14:05:32 +0000

    Adding comments to .ino file

--- arduino/firebase_random_num/firebase_random_num.ino
+++ arduino/firebase_random_num/firebase_random_num.ino
@@ -1,11 +1,32 @@
+/*
+Joshua Geoghegan
+JG20251202
+TU816, B.Eng Computer Engineering
+Stage 4
+Project
+Suprvisor: Mark Deegan
+
+Code to test sending a random value, pieve of data, tyo FireBase
+Firebase is the database
+That I have set up for testing software steps as part of my 4th Year project
+
+*/
+
+// Need these to use the WiFi functions
 #include <WiFi.h>
+// to send and receive data over the internet, we are going to use this for communication with FireBase
 #include <HTTPClient.h>
+// Converting something to JSON before sending, because firebase likes to receive (and return ) JSON
 #include <ArduinoJson.h>
 
 const char* ssid = "DESKTOP-LPIC61A";
 const char* password = "12345678";
 
 const char* firebaseURL = "https://firestore.googleapis.com/v1/projects/smart-hurley-test/databases/(default)/documents/test_data?key=AIzaSyDp1ep5j_90oInc9v2YJf1RKNG2cHG13WE";
+// const char* justThe URL = 
+// const char* justTheInstance =
+// const Char* the APIKey =
+
 
 void setup() {
   Serial.begin(115200);
@@ -19,6 +40,7 @@ void setup() {
 }
 
 void loop() {
+  // how about some comments, for a start?
   if(WiFi.status() == WL_CONNECTED){
     HTTPClient http;
 
